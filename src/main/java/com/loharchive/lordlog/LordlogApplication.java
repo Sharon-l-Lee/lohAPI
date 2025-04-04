fd216f3eb225aac3246fd2557eb230ef63f01652
sharon-lee <rosharonl22@gmail.com>
2025-04-04 18:08:36 +0900

    프로젝트 생성

---
+++ src/main/java/com/loharchive/lordlog/LordlogApplication.java
@@ -0,0 +1,13 @@
+package com.loharchive.lordlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LordlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loharchive/lordlog/LordlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loharchive.lordlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LordlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
